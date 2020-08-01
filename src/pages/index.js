c40a41a855af936e58f686f093102a353c5c17ba
hoang21 <hoang21@gmail.com>
2020-08-01 15:56:57 +1000

    first

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../components/layout'
+import Metadata from '../components/metadata'
+
+const Index = () => {
+  return (
+    <Layout>
+      <Metadata title='Home' description='This is my home page' />
+    </Layout>
+  )
+}
+
+export default Index
